2886199076cd15cdad54072dec87235801a6b279
Radomir Kolev <radomir98@abv.bg>
2020-01-30 17:03:48 +0200

    Refactor MazePoint

--- Plugins/Maze/Source/Maze/Private/Rectangle.cpp
+++ Plugins/Maze/Source/Maze/Private/Rectangle.cpp
@@ -6,8 +6,8 @@ int FRectangle::clamp(int v, int lo, int hi) {
 	if (lo > hi) Swap<int>(lo, hi);
 	return (v < lo) ? lo : (hi < v) ? hi : v;
 }
-MazePoint FRectangle::getBottomLeft() const {
-	return MazePoint(topLeft.x + width, topLeft.y + height);
+FMazePoint FRectangle::getBottomLeft() const {
+	return FMazePoint(topLeft.x + width, topLeft.y + height);
 }
 bool FRectangle::intersectsWith(const FRectangle& r) const
 {
@@ -17,25 +17,25 @@ bool FRectangle::intersectsWith(const FRectangle& r) const
 		topLeft.x >= r.topLeft.x + r.width ||
 		topLeft.y >= r.topLeft.y + r.height);
 }
-MazePoint FRectangle::getNearestPointFrom(const FRectangle& r) const
+FMazePoint FRectangle::getNearestPointFrom(const FRectangle& r) const
 {
 	int newXR2 = clamp(topLeft.x, r.topLeft.x, r.topLeft.x + r.width - 1);
 	int newYR2 = clamp(topLeft.y, r.topLeft.y, r.topLeft.y + r.height - 1);
-	return MazePoint(newXR2, newYR2);
+	return FMazePoint(newXR2, newYR2);
 }
 
-MazePoint FRectangle::getNearestPointTo(const FRectangle& r) const
+FMazePoint FRectangle::getNearestPointTo(const FRectangle& r) const
 {
-	MazePoint p = getNearestPointFrom(r);
+	FMazePoint p = getNearestPointFrom(r);
 
 	int newXR1 = clamp(p.x, topLeft.x, topLeft.x + width - 1);
 	int newYR1 = clamp(p.y, topLeft.y, topLeft.y + height - 1);
 
-	return MazePoint(newXR1, newYR1);
+	return FMazePoint(newXR1, newYR1);
 }
 uint64 FRectangle::getDistanceSquaredTo(FRectangle r2) const
 {
-	return MazePoint::getDistanceSquared(getNearestPointTo(r2), getNearestPointFrom(r2));
+	return FMazePoint::getDistanceSquared(getNearestPointTo(r2), getNearestPointFrom(r2));
 }
 uint64 FRectangle::getDistanceToCenterSquared() const
 {
@@ -49,15 +49,15 @@ bool FRectangle::operator<=(const FRectangle r2) const {
 	return getDistanceToCenterSquared() <= r2.getDistanceToCenterSquared();
 }
 
-bool FRectangle::containsPoint(const MazePoint& p1) const
+bool FRectangle::containsPoint(const FMazePoint& p1) const
 {
 	return p1.x >= topLeft.x && p1.x <= topLeft.x + width - 1 &&
 		p1.y >= topLeft.y && p1.y <= topLeft.y + height - 1;
 }
 FRectangle FRectangle::getIntersectionRectangle(const FRectangle& r2) const {
 	if (!intersectsWith(r2)) return FRectangle(0, 0, -1, -1);
-	MazePoint p1 = getNearestPointFrom(r2);
-	MazePoint r2Down = r2.getBottomLeft();
+	FMazePoint p1 = getNearestPointFrom(r2);
+	FMazePoint r2Down = r2.getBottomLeft();
 	if (containsPoint(r2Down)) {
 		return FRectangle(p1.x, p1.y, r2Down.x - p1.x, r2Down.y - p1.y);
 	}
--- Plugins/Maze/Source/Maze/Public/Rectangle.h
+++ Plugins/Maze/Source/Maze/Public/Rectangle.h
@@ -3,40 +3,50 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
+#include "Rectangle.generated.h"
 /**
  * 
  */
-struct MAZE_API MazePoint {
-	int x;
-	int y;
-	MazePoint(int x, int y) :x(x), y(y) {}
+USTRUCT()
+struct MAZE_API FMazePoint {
+	GENERATED_BODY()
 
-	static int getDistanceSquared(const MazePoint p1, const MazePoint p2) {
+	UPROPERTY(EditAnywhere)
+	int x;
+	UPROPERTY(EditAnywhere)
+	int y;
+
+	FMazePoint(int x, int y) :x(x), y(y) {}
+	FMazePoint() = default;
+	static int getDistanceSquared(const FMazePoint p1, const FMazePoint p2) {
 		return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
 	}
-	bool operator==(const MazePoint& p) const {
+	bool operator==(const FMazePoint& p) const {
 		return x == p.x && y == p.y;
 	}
 };
+inline uint32 GetTypeHash(const FMazePoint& mp)
+{
+	return mp.x * 1001 + mp.y * 1003;
+}
 
 class MAZE_API FRectangle
 {
 private:
-	MazePoint topLeft;
+	FMazePoint topLeft;
 	int width;
 	int height;
 public:
 	static int clamp(int, int, int);
-	MazePoint getBottomLeft() const;
+	FMazePoint getBottomLeft() const;
 	bool intersectsWith(const FRectangle& r) const;
-	MazePoint getNearestPointFrom(const FRectangle& r) const;
-	MazePoint getNearestPointTo(const FRectangle& r) const;
+	FMazePoint getNearestPointFrom(const FRectangle& r) const;
+	FMazePoint getNearestPointTo(const FRectangle& r) const;
 	uint64 getDistanceSquaredTo(FRectangle r2) const;
 	uint64 getDistanceToCenterSquared() const;
 	bool operator<(const FRectangle r2) const;
 	bool operator<=(const FRectangle r2) const;
-	bool containsPoint(const MazePoint&) const;
+	bool containsPoint(const FMazePoint&) const;
 	FRectangle getIntersectionRectangle(const FRectangle&) const;
 	FRectangle(int, int, int, int);
 	FRectangle() = default;
