291d9fbfb4ee5c6ab5b504e8b0418c4381c36b5c
Radomir Kolev <radomir98@abv.bg>
2020-02-06 20:41:34 +0200

    Refactor MazeScalingBuilder

--- Plugins/Maze/Source/Maze/Private/MazeScalingBuilder.cpp
+++ Plugins/Maze/Source/Maze/Private/MazeScalingBuilder.cpp
@@ -29,29 +29,98 @@ void AMazeScalingBuilder::BuildMaze()
 	// TO DO;
 }
 
-void AMazeScalingBuilder::BuildMazeFromScheme(const TArray<TArray<int8>>& MazeScheme)
+void AMazeScalingBuilder::BuildMazeFromScheme( TArray<TArray<int8>> MazeScheme)
 {
 	UE_LOG(Maze, Error, TEXT("HERE! %d"),MazeScheme.Num());
 	for (int i = 0; i < MazeScheme.Num(); ++i)
 	{
 		for (int j = 0; j < MazeScheme[i].Num(); ++j)
 		{
-			if (CharacterMap.Find(MazeScheme[i][j]) != nullptr)
+			if (MazeScheme[i][j] != 0)
 			{
-				UE_LOG(Maze, Warning, TEXT("Building actor on coordinates %d,%d"), i, j);
-				auto ComponentInformation = CharacterMap.FindChecked(MazeScheme[i][j]);
-				auto InstanceName = ComponentInformation.InstanceMeshName;
-				if (InstanceMeshes.Find(InstanceName) != nullptr)
+				if (CharacterMap.Find(MazeScheme[i][j]) != nullptr)
 				{
-					auto Instance = InstanceMeshes.FindChecked(InstanceName);
-					FVector InstanceScale = FVector(ComponentInformation.Scale.X*Basis.Scale.X, ComponentInformation.Scale.Y * Basis.Scale.Y, ComponentInformation.Scale.Z * Basis.Scale.Z);
-					FTransform Trans( FRotator::ZeroRotator,Basis.GetMazeActorLocation(FIntPoint(i, j),ComponentInformation.Scale),InstanceScale);
-					Instance->AddInstanceWorldSpace(Trans);
+					UE_LOG(Maze, Warning, TEXT("Building actor on coordinates %d,%d"), i, j);
+					auto ComponentInformation = CharacterMap.FindChecked(MazeScheme[i][j]);
+					auto InstanceName = ComponentInformation.InstanceMeshName;
+					if (InstanceMeshes.Find(InstanceName) != nullptr)
+					{
+						int8 ID = MazeScheme[i][j];
+						int32 NumberRight = 0;
+						int32 NumberDown = 0;
+						bool bContinueRight = true;
+						bool bContinueDown = true;
+						//bool bRectangle = true;
+						while (bContinueRight || bContinueDown)
+						{
+							if (i+NumberDown >= MazeScheme.Num() )
+							{
+								bContinueDown = false;
+							}
+							if (j + NumberRight >= MazeScheme[i].Num())
+							{
+								bContinueRight = false;
+							}
+							/*if (bContinueDown && bContinueRight)
+							{
+								if (MazeScheme[i + NumberDown][j + NumberRight] != ID)
+									bContinueRight = false;
+							}*/
+							if (bContinueRight)
+							{
+								for (int k = i; k < i + NumberDown && k < MazeScheme.Num(); ++k)
+								{
+									if (MazeScheme[k][j + NumberRight] != ID)
+									{
+										bContinueRight = false;
+										break;
+									}
+								}
+								if (bContinueRight)
+								{
+									NumberRight++;
+								}
+							}
+							if (bContinueDown)
+							{
+								for (int k = j; k < j + NumberRight && k < MazeScheme[i].Num(); ++k)
+								{
+									if (MazeScheme[i+NumberDown][k] != ID)
+									{
+										bContinueDown = false;
+										break;
+									}
+								}
+								if (bContinueDown)
+								{
+									NumberDown++;
+								}
+							}
+							/*if (bContinueRight && bContinueDown)
+							{
+								if (MazeScheme[i + NumberDown][j + NumberRight] != ID)
+									bContinueRight = false;
+							}*/
+						}
+						for (int k = i; k < i + NumberDown; ++k)
+						{
+							for (int l = j; l < j + NumberRight; ++l)
+							{
+								MazeScheme[k][l] = 0;
+							}
+						}
+						UE_LOG(Maze, Log, TEXT("Maze Component with size:%d,%d"), NumberRight, NumberDown);
+						auto Instance = InstanceMeshes.FindChecked(InstanceName);
+						FIntVector InstanceScaleInt = FIntVector(ComponentInformation.Scale.X * (NumberDown), ComponentInformation.Scale.Y * (NumberRight), ComponentInformation.Scale.Z);
+						FVector InstanceScale = FVector(ComponentInformation.Scale.X * Basis.Scale.X* (NumberDown), ComponentInformation.Scale.Y * Basis.Scale.Y * (NumberRight), ComponentInformation.Scale.Z * Basis.Scale.Z);
+						FTransform Trans(FRotator::ZeroRotator, Basis.GetMazeActorLocation(FIntPoint(i, j), InstanceScaleInt), InstanceScale);
+						Instance->AddInstanceWorldSpace(Trans);
+					}
 				}
-			}
-			else
-			{
-				UE_LOG(Maze, Error, TEXT("No valid component exists for this code: %d"), MazeScheme[i][j]);
+				else
+				{
+					UE_LOG(Maze, Error, TEXT("No valid component exists for this code: %d"), MazeScheme[i][j]);
+				}
 			}
 		}
 	}
